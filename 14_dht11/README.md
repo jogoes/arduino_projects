1455183ce50b9ff14f8261a1a5ba2cde66a8410f
Josef <josef.goebel@posteo.de>
2017-02-16 23:06:20 +0100

    added DHT11 sample project

---
+++ 14_dht11/README.md
@@ -0,0 +1,18 @@
+## DHT11 temperature and humidity sensor with Arduino
+
+Simple DHT11 temperature and humidity sensor demo
+showing usage of sensor similar to the one e.g. shown here:
+https://www.adafruit.com/product/386
+
+Code is using the Adafruit DHT11 library and basically
+a simplified version of the DHTTester sample code coming with this
+library.
+
+Connections:
+
+| Arduino   | DHT11
+------------|---------
+| 11        | DATA
+| GND       | GND
+| VCC (+5V) | VCC
+
---
+++ 14_dht11/dht11/dht11.ino
@@ -0,0 +1,70 @@
+/**
+ * Simple DHT11 temperature and humidity sensor demo
+ * showing usage of sensor similar to the one e.g. shown here:
+ * https://www.adafruit.com/product/386
+ * 
+ * Code is using the Adafruit DHT11 library and basically
+ * a simplified version of the DHTTester sample code coming with this
+ * library.
+ * 
+ * Connections:
+ * 
+ * Arduino   | DHT11
+ * --------------------
+ * 11        | DATA
+ * GND       | GND
+ * VCC (+5V) | VCC
+ */
+#include "DHT.h"
+
+#define DHTPIN 11     // what digital pin we're connected to
+
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11   // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DHTxx test!");
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
+  Serial.println(" *F");
+}
